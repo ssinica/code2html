4460603d1c3c8f188acc6b354ee769366c989c62
Sergey Sinica <sergey.sinica@odnoklassniki.ru>
2012-05-22 17:14:53 +0300

    new line

--- src/com/ss/code2html/engine/formatter/AbstractFormatCtx.java
+++ src/com/ss/code2html/engine/formatter/AbstractFormatCtx.java
@@ -3,10 +3,12 @@ package com.ss.code2html.engine.formatter;
 public class AbstractFormatCtx implements FormatCtx {
 
 	private StringBuffer buf = new StringBuffer();
+	private static final String NEW_LINE = System.getProperty("line.separator");
 
 	@Override
 	public void appendFormattedLine(String formattedLine) {
 		buf.append(formattedLine);
+		buf.append(NEW_LINE);
 	}
 
 	@Override
